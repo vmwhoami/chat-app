61c28b6cd75e201df40e22ec9726cc2abd6883e6
Vitalie Melnic <vmwhoami@gmail.com>
2020-09-27 09:26:37 +0300

    Add custom error message div

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'login',to:'session#new'
   post 'login',to:'session#create'
-  get 'logout',to: 'session#destroy'
+  get 'logout',to: 'session#destroy', as: 'logout'
 root 'chatroom#index'
 resources :users
  
