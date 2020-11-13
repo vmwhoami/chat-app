eed204fb3deb55043e7cc722f6510d13d08d68d6
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-13 10:27:46 +0200

    put the logic inside application helper

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,17 @@
 class ChatroomController < ApplicationController
+
+  before_action :logged_in_user
+
+
   def index
     @messages = Message.all
   end
+
+  private
+
+  def logged_in_user
+    redirect_to login_path unless current_user
+  end
+  
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,19 @@ module ApplicationHelper
   end
 
   def login_btn
-  link_to  "Login", login_path,class:"item"   
+     link_to  "Login", login_path,class:"item"   
   end
-  def logout_btn
-    
+
+
+  def menu_btns(current_user)
+    if current_user
+      link_to  "Logout", logout_path, method: :delete ,class:"item" 
+    else
+      (link_to  "Login", login_path,class:"item" )+
+      (link_to  "Register", new_user_path, class:"item")
+    end
   end
+
+
 
 end
