f2e70a0d9117c5b09235540e634146c580787aea
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-14 19:18:17 +0200

    add distance to time in words

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
-before_save :set_def_img
+  after_initialize :set_def_img
 validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum:4, maximum: 50}
 validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum:4}
 
 has_many :messages 
 has_secure_password
 
+
 def set_def_img
-  self.main_img||= "https://via.placeholder.com/140x140"
+@images = [
+  "http://alices-html.themerex.net/images/product-12-300x410.jpg",
+  "http://alices-html.themerex.net/images/product-17-300x410.jpg",
+  "http://alices-html.themerex.net/images/product-19-300x410.jpg",
+  "http://alices-html.themerex.net/images/product-13.jpg"
+]
+  self.main_img||= @images.sample
 end
 
 end
